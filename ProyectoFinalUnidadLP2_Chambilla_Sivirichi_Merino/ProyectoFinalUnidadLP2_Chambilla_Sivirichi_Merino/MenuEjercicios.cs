8430d8d3df0f72a8de695b59b1619d69e48ac17e
AlmendraMQ <katmerinoq@upt.pe>
2020-06-08 12:41:25 -0500

    version2

---
+++ ProyectoFinalUnidadLP2_Chambilla_Sivirichi_Merino/ProyectoFinalUnidadLP2_Chambilla_Sivirichi_Merino/MenuEjercicios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalUnidadLP2_Chambilla_Sivirichi_Merino
+{
+    public partial class MenuEjercicios : Form
+    {
+        public MenuEjercicios()
+        {
+            InitializeComponent();
+        }
+    }
+}
